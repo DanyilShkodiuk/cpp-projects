7736f0446e4bed74013e49b6356ef41196bf7b2c
Danyil Shkodiuk <danil.shkodiuk@gmail.com>
2025-12-17 16:24:43 +0100

    while tic tak toe

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,82 +1,92 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-void wypiszTabele(char tabela[3][3]) {
+void wypiszPlansze(char plansza[3][3]) {
     for (int i = 0; i < 3; i++) {
         cout << "   |   |   " << endl;
-        cout << " " << tabela[i][0] << " | " << tabela[i][1] << " | " << tabela[i][2] << endl;
-
-        if (i < 2) {
-            cout << "___|___|___" << endl;
-        }
+        cout << " " << plansza[i][0] << " | " << plansza[i][1] << " | " << plansza[i][2] << endl;
+        if (i < 2) cout << "___|___|___" << endl;
     }
     cout << "   |   |   " << endl;
 }
 
-main() {
-    char tabela[3][3] = {
-        {' ', ' ', ' '},
-        {' ', ' ', ' '},
-        {' ', ' ', ' '}
-    };
+int main() {
+    char wybor;
 
-    const char graczX = 'X';
-    const char graczO = 'O';
-    char terazGra = graczX;
-    int w,k = -1;
-    char zwyciezca = ' ';
+    do {
+        // reset planszy
+        char plansza[3][3] = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+        };
 
-    for (int i = 0; i < 9; i++) {
-        wypiszTabele(tabela);
+        const char graczX = 'X';
+        const char graczO = 'O';
+        char terazGra = graczX;
+        char zwyciezca = ' ';
+        int w = -1, k = -1;
 
-        if(zwyciezca != ' ') {
-            break;
-        }
+        for (int i = 0; i < 9; i++) {
+            wypiszPlansze(plansza);
 
-        cout << "Teraz gra: " << terazGra << endl;
-        while(true) {
-            cout << "Wprowadz w & k od 0 do 2 dla wiersza i kolumny: ";
-            cin >> w >> k;
-            if (w < 0 || w > 2 || k < 0 || k > 2) {
-                cout << "Niepoprawna liczba, sprobuj ponownie." << endl;
-            }else if (tabela[w][k] != ' ') {
-                cout << "Pole wypelnione, sprobuj ponownie" << endl;
-            }else {
-                break;
+            if (zwyciezca != ' '){break;} 
+
+            cout << "Teraz gra: " << terazGra << endl;
+
+            while (true) {
+                cout << "Wprowadz w & k od 0 do 2: ";
+                cin >> w >> k;
+
+                if (!cin || w < 0 || w > 2 || k < 0 || k > 2) {
+                    cout << "Niepoprawne dane!" << endl;
+                }
+                else if (plansza[w][k] != ' ') {
+                    cout << "Pole zajete!" << endl;
+                }
+                else break;
+
+                cin.clear();
+                cin.ignore(10000, '\n');
             }
-            w,k = -1;
-            cin.clear();
-            cin.ignore(10000, '\n');
-        }
 
-        tabela[w][k] = terazGra;
-        terazGra = (terazGra == graczX) ? graczO : graczX;
+            plansza[w][k] = terazGra;
+            terazGra = (terazGra == graczX) ? graczO : graczX;
 
-        for (int w = 0; w < 3; w++) {
-            if (tabela[w][0] != ' ' && tabela[w][0] == tabela[w][1] && tabela[w][1] == tabela[w][2]){
-                zwyciezca = tabela[w][0];
-                break;
+            // wiersze
+            for (int r = 0; r < 3; r++){
+                if (plansza[r][0] != ' ' && plansza[r][0] == plansza[r][1] && plansza[r][1] == plansza[r][2])
+                    zwyciezca = plansza[r][0];
+            }
+
+            // kolumny
+            for (int c = 0; c < 3; c++){
+                if (plansza[0][c] != ' ' && plansza[0][c] == plansza[1][c] && plansza[1][c] == plansza[2][c])
+                    zwyciezca = plansza[0][c];
+            }
+
+            // przekatne
+            if (plansza[0][0] != ' ' && plansza[0][0] == plansza[1][1] && plansza[1][1] == plansza[2][2]){
+                zwyciezca = plansza[0][0];
+            }
+            if (plansza[0][2] != ' ' && plansza[0][2] == plansza[1][1] && plansza[1][1] == plansza[2][0]){
+                zwyciezca = plansza[0][2];
             }
         }
 
-        for (int k = 0; k < 3; k++) {
-            if (tabela[0][k] != ' ' && tabela[0][k] == tabela[1][k] && tabela[1][k] == tabela[2][k]){
-                zwyciezca = tabela[0][k];
-                break;
-            }
+        wypiszPlansze(plansza);
+
+        if (zwyciezca != ' '){
+            cout << "Gracz " << zwyciezca << " wygral!\n";
+        }
+        else {
+            cout << "Remis!\n";
         }
 
-        if(tabela[0][0] != ' ' && tabela[0][0] == tabela[1][1] && tabela[1][1] == tabela[2][2]) {
-            zwyciezca = tabela[0][0];
-        }
-        else if (tabela[0][2] != ' ' && tabela[0][2] == tabela[1][1] && tabela[1][1] == tabela[2][0]){
-            zwyciezca = tabela[0][2];
-        }
-    }
-    if(zwyciezca != ' ') {
-        cout << "Gracz " << zwyciezca << " wygral!" << endl;
-    }else {
-        cout << "Remis!" << endl;
-    }
+        cout << "Gramy dalej [t/n]: ";
+        cin >> wybor;
+
+    } while (wybor == 't' || wybor == 'T');
+
+    return 0;
 }
