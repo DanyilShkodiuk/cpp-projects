75d2252e38f661819510387cd8a85fb4e2484fd8
Danyil Shkodiuk <danil.shkodiuk@gmail.com>
2025-12-22 17:59:55 +0100

    matrix

--- otherProj/main.cpp
+++ otherProj/main.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
-#include <array>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 
 int main() {
-    int N = 7;
-    int temperatura[N];
-    int suma = 0;
-    cout << "Wprowadz 7 losowych temperatur" << endl << endl;
-    for (int i = 0; i < N; i++){
-        cout << "Temperatura " << i+1 << ": ";
-        cin >> temperatura[i];
-        suma += temperatura[i];
-    }
-    cout << endl;
-    int srednia = suma / N;
-    cout << "Srednia ocen: " << srednia << endl << endl;
+    int x = time(0);
+    int N = 4;
+    int matrix[N][N];
+    
+    srand(x);
 
-    cout << "Wieksza od sredniej: "; 
-    for (int i = 0; i < N; i++) {
-        if(temperatura[i] > srednia){
-            cout << temperatura[i] << " ";
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++) {
+            matrix[i][j] = 1 + rand() % 9;
         }
     }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << matrix[i][j] << " ";       
+        }
+        cout << endl;
+    }
+
+    int sumMain = 0;
+    int sumSecondary = 0;
+    for (int i = 0; i < N; i++) {
+        sumMain += matrix[i][i];
+        sumSecondary += matrix[i][N - 1 - i];
+    }
+
+    cout << "Suma przekatnej glownej: " << sumMain << endl;
+    cout << "Suma przekatnej bocznej: " << sumSecondary << endl;
+    return 0;
 }
